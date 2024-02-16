65f7fd30d7f4cb76c10d16681f8b872e421165a3
Kofichamp <117198167+Kofichamp@users.noreply.github.com>
2024-02-16 14:02:32 +0200

    Update settings.py

--- historyArchive/settings.py
+++ historyArchive/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/MyHistoryArchive/static'
+STATIC_URL = '/home/ubuntu/MyHistoryArchive/static/'
 
 # settings.py
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
